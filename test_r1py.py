3de8910bfa28a579903ebd9761bb4cb87c520878
Lachlan Cresswell <lachy93@gmail.com>
2021-08-12 00:49:53 +1000

    Further testing + create test project fixture

--- test_r1py.py
+++ test_r1py.py
@@ -9,26 +9,56 @@ from shutil import copyfile
 TEMP_FILE = './templates.r2t'
 DIRTY_FILE = './dirty.dbpr'
 CLEAN_FILE = './clean.dbpr'
-DEFAULT_FILE = './default.dbpr'
+TEST_FILE = './TEST.dbpr'
 
 
-def test_cleanProjectFile():
-    copyfile(DEFAULT_FILE, DIRTY_FILE)
-    dirtyProj = r1.ProjectFile(DIRTY_FILE)
-    tFile = r1.TemplateFile(TEMP_FILE)
+def test_loadTemplateFailure():
+    with pytest.raises(sqlite3.OperationalError):
+        r1.TemplateFile('./tempilates.r2t')
 
-    assert type(dirtyProj) is r1.ProjectFile
-    assert type(tFile) is r1.TemplateFile
 
-    initGrpCount = dirtyProj.getGroupCount()
+def test_loadTemplateSuccess():
+    template = r1.TemplateFile(TEMP_FILE)
+    assert type(template) is r1.TemplateFile
 
-    dirtyProj.pId = dirtyProj.createGroup(autor1.PARENT_GROUP_TITLE, 1)[0]
-    dirtyProj.setSrcGrpInfo()
-    autor1.configureApChannels(dirtyProj)
 
-    autor1.createMeterView(dirtyProj, tFile)
-    autor1.createMasterView(dirtyProj, tFile)
-    autor1.createNavButtons(dirtyProj, tFile)
+def test_loadProjectFailure():
+    with pytest.raises(ValueError):
+        r1.ProjectFile('./tempilates.r2t')
+
+
+def test_loadProjectSuccess():
+    assert type(r1.ProjectFile(TEST_FILE)) is r1.ProjectFile
+
+
+@pytest.fixture(scope="module")
+def loadedProject():
+    copyfile(TEST_FILE, DIRTY_FILE)
+    return r1.ProjectFile(DIRTY_FILE)
+
+
+def test_getViewIdFromName(loadedProject):
+    with pytest.raises(RuntimeError):
+        loadedProject.getViewIdFromName("abcd")
+    assert loadedProject.getViewIdFromName("Overview") >= 1000
+
+
+def test_cleanProjectFile(loadedProject):
+    template = r1.TemplateFile(TEMP_FILE)
+
+    assert type(loadedProject) is r1.ProjectFile
+    assert type(template) is r1.TemplateFile
+
+    initGrpCount = loadedProject.getGroupCount()
+
+    loadedProject.pId = loadedProject.createGrp(
+        autor1.PARENT_GROUP_TITLE, 1)[0]
+    loadedProject.setSrcGrpInfo()
+    autor1.configureApChannels(loadedProject)
+
+    autor1.createMeterView(loadedProject, template)
+    autor1.createMasterView(loadedProject, template)
+    autor1.createNavButtons(loadedProject, template)
 
     queries = [
         # Master view
@@ -38,24 +68,24 @@ def test_cleanProjectFile():
         # AutoR1 group
         f'SELECT * FROM Groups WHERE Name = "{autor1.PARENT_GROUP_TITLE}"',
         # Nav controls
-        f'SELECT * FROM Controls WHERE TargetId = {dirtyProj.masterViewId}',
+        f'SELECT * FROM Controls WHERE TargetId = {loadedProject.masterViewId}',
         # Master view controls
-        f'SELECT * FROM Controls WHERE ViewId = {dirtyProj.masterViewId}',
+        f'SELECT * FROM Controls WHERE ViewId = {loadedProject.masterViewId}',
         # Meter view controls
-        f'SELECT * FROM Controls WHERE ViewId = {dirtyProj.meterViewId}',
+        f'SELECT * FROM Controls WHERE ViewId = {loadedProject.meterViewId}',
     ]
 
     for q in queries:
-        dirtyProj.cursor.execute(q)
-        assert dirtyProj.cursor.fetchone() is not None
+        loadedProject.cursor.execute(q)
+        assert loadedProject.cursor.fetchone() is not None
 
-    dirtyProj.close()
+    loadedProject.close()
     copyfile(DIRTY_FILE, CLEAN_FILE)
 
     cleanProj = r1.ProjectFile(CLEAN_FILE)
-    cleanProj.pId = dirtyProj.pId
+    cleanProj.pId = loadedProject.pId
     cleanProj = autor1.clean(
-        cleanProj, dirtyProj.masterViewId, dirtyProj.meterViewId)
+        cleanProj, loadedProject.masterViewId, loadedProject.meterViewId)
     postGrpCount = cleanProj.getGroupCount()
     assert postGrpCount == initGrpCount
 
@@ -64,23 +94,3 @@ def test_cleanProjectFile():
         assert cleanProj.cursor.fetchone() is None
 
     cleanProj.close()
-
-
-def test_loadTemplateFailure():
-    with pytest.raises(sqlite3.OperationalError):
-        r1.TemplateFile('./tempilates.r2t')
-
-
-def test_loadTemplateSuccess():
-    tempFile = r1.TemplateFile(TEMP_FILE)
-    assert type(tempFile) is r1.TemplateFile
-
-
-def test_loadProjectFailure():
-    with pytest.raises(ValueError):
-        r1.ProjectFile('./tempilates.r2t')
-
-
-def test_loadProjectSuccess():
-    projFile = r1.ProjectFile(DEFAULT_FILE)
-    assert type(projFile) is r1.ProjectFile
