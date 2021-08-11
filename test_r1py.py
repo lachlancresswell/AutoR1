d7a6b2e813d65619e4c71deb9a92f2054bb8791c
Lachlan Cresswell <lachy93@gmail.com>
2021-08-11 23:27:37 +1000

    Update tests to new file layout

--- test_r1py.py
+++ test_r1py.py
@@ -3,57 +3,67 @@ import sqlite3
 import sys
 import pytest
 import r1py as r1
+import autor1
 from shutil import copyfile
 
 TEMP_FILE = './templates.r2t'
-PROJ_FILE = './default.dbpr'
-TEST_FILE = './test.dbpr'
+DIRTY_FILE = './dirty.dbpr'
+CLEAN_FILE = './clean.dbpr'
+DEFAULT_FILE = './default.dbpr'
 
 
 def test_cleanProjectFile():
-
-    pFile = r1.ProjectFile(TEST_FILE)
+    copyfile(DEFAULT_FILE, DIRTY_FILE)
+    dirtyProj = r1.ProjectFile(DIRTY_FILE)
     tFile = r1.TemplateFile(TEMP_FILE)
 
-    assert type(pFile) is r1.ProjectFile
+    assert type(dirtyProj) is r1.ProjectFile
     assert type(tFile) is r1.TemplateFile
 
-    initGrpCount = pFile.getGroupCount()
+    initGrpCount = dirtyProj.getGroupCount()
 
-    pFile.pId = pFile.createGroup(r1.PARENT_GROUP_TITLE, 1)[0]
-    pFile.setSrcGrpInfo()
-    pFile.configureApChannels()
+    dirtyProj.pId = dirtyProj.createGroup(autor1.PARENT_GROUP_TITLE, 1)[0]
+    dirtyProj.setSrcGrpInfo()
+    autor1.configureApChannels(dirtyProj)
 
-    r1.createMeterView(pFile, tFile)
-    r1.createMasterView(pFile, tFile)
-    r1.createNavButtons(pFile, tFile)
+    autor1.createMeterView(dirtyProj, tFile)
+    autor1.createMasterView(dirtyProj, tFile)
+    autor1.createNavButtons(dirtyProj, tFile)
 
     queries = [
         # Master view
-        f'SELECT * FROM Views WHERE "Name" = "{r1.MASTER_WINDOW_TITLE}"',
+        f'SELECT * FROM Views WHERE "Name" = "{autor1.MASTER_WINDOW_TITLE}"',
         # Meter view
-        f'SELECT * FROM Views WHERE "Name" = "{r1.METER_WINDOW_TITLE}"',
+        f'SELECT * FROM Views WHERE "Name" = "{autor1.METER_WINDOW_TITLE}"',
         # AutoR1 group
-        f'SELECT * FROM Groups WHERE Name = "{r1.PARENT_GROUP_TITLE}"',
+        f'SELECT * FROM Groups WHERE Name = "{autor1.PARENT_GROUP_TITLE}"',
         # Nav controls
-        f'SELECT * FROM Controls WHERE TargetId = {pFile.masterViewId}',
+        f'SELECT * FROM Controls WHERE TargetId = {dirtyProj.masterViewId}',
         # Master view controls
-        f'SELECT * FROM Controls WHERE ViewId = {pFile.masterViewId}',
+        f'SELECT * FROM Controls WHERE ViewId = {dirtyProj.masterViewId}',
         # Meter view controls
-        f'SELECT * FROM Controls WHERE ViewId = {pFile.meterViewId}',
+        f'SELECT * FROM Controls WHERE ViewId = {dirtyProj.meterViewId}',
     ]
 
     for q in queries:
-        pFile.cursor.execute(q)
-        assert pFile.cursor.fetchone() is not None
+        dirtyProj.cursor.execute(q)
+        assert dirtyProj.cursor.fetchone() is not None
 
-    pFile.clean()
-    postGrpCount = pFile.getGroupCount()
+    dirtyProj.close()
+    copyfile(DIRTY_FILE, CLEAN_FILE)
+
+    cleanProj = r1.ProjectFile(CLEAN_FILE)
+    cleanProj.pId = dirtyProj.pId
+    cleanProj = autor1.clean(
+        cleanProj, dirtyProj.masterViewId, dirtyProj.meterViewId)
+    postGrpCount = cleanProj.getGroupCount()
     assert postGrpCount == initGrpCount
 
     for q in queries:
-        pFile.cursor.execute(q)
-        assert pFile.cursor.fetchone() is None
+        cleanProj.cursor.execute(q)
+        assert cleanProj.cursor.fetchone() is None
+
+    cleanProj.close()
 
 
 def test_loadTemplateFailure():
@@ -72,5 +82,5 @@ def test_loadProjectFailure():
 
 
 def test_loadProjectSuccess():
-    projFile = r1.ProjectFile(PROJ_FILE)
+    projFile = r1.ProjectFile(DEFAULT_FILE)
     assert type(projFile) is r1.ProjectFile
