1f10f73338abd10c23cfa136eb2447d57247c3df
Lachlan Cresswell <lachy93@gmail.com>
2021-08-17 23:48:23 +1000

    ci-cd initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(name='misha_math',
+      version='1.7.0',
+      description='Automated view and control generator for d&b audiotechnik .dbpr files',
+      author='Lachlan',
+      packages=['r1py', 'autor1'])
