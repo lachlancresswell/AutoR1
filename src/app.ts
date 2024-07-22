09011e7d771f82a73fed7efc98051f1ae1c9544f
Lachlan Cresswell <lachlancresswell@gmail.com>
2024-07-22 07:31:21 -0400

    feat (app): support EQ group and add UI tips

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const SUFFIX = '_AUTO';
 
 const DEFAULT_INPUT_GAIN_TYPE = AutoR1.INPUT_GAIN_TYPE.ANALOG;
 
+let defaultTemplates: AutoR1.AutoR1TemplateFile;
 let templates: AutoR1.AutoR1TemplateFile;
 
 (window as any).VERSION = process.env.VERSION;
@@ -17,6 +18,7 @@ interface SourceGroupStatus {
     fallback: boolean;
     mute: boolean;
     ds: boolean;
+    eq: boolean;
 }
 
 const DEFAULT_OPTIONS: AutoR1.ProjectOptions = Object.freeze({
@@ -29,15 +31,21 @@ const DEFAULT_OPTIONS: AutoR1.ProjectOptions = Object.freeze({
 
 interface AppData {
     projectFile: AutoR1.AutoR1ProjectFile | undefined;
+    customTemplateFileName: string;
     fileName: string | undefined;
     sourceGroupsStatus: SourceGroupStatus[] | undefined;
     projectOptions: AutoR1.ProjectOptions;
     fileBuffer: Buffer | undefined;
     isVisible: boolean;
     clearFile: () => void;
+    clearCustomTemplateFile: () => void;
 }
 
+const handleValueToggle = (val: boolean | number, ref: HTMLElement) => val ? ref.classList.add('bg-slate-200') : ref.classList.remove('bg-slate-200');
+
 const newAutoPath = (path: string) => path.substring(0, path.lastIndexOf('.')) + SUFFIX + '.dbpr';
+
+const newCleanPath = (path: string) => path.substring(0, path.lastIndexOf('.')) + 'CLEAN' + '.dbpr';
 
 const downloadFile = (projectFile: AutoR1.AutoR1ProjectFile, filename: string) => {
     const arraybuff = projectFile.db.export();
@@ -45,7 +53,7 @@ const downloadFile = (projectFile: AutoR1.AutoR1ProjectFile, filename: string) =
     var blob = new Blob([arraybuff]);
     const link = window.URL.createObjectURL(blob);
 
-    var a = document.createElement("a");
+    const a = document.createElement("a");
     document.body.appendChild(a);
     a.href = link;
     a.download = filename;
@@ -72,6 +80,7 @@ const processFile = async (fileBuffer: Buffer, status: SourceGroupStatus[], opti
         projectFile.sourceGroups[i].fallback = row.fallback;
         projectFile.sourceGroups[i].mute = row.mute;
         projectFile.sourceGroups[i].dsData = row.ds;
+        projectFile.sourceGroups[i].eq = row.eq;
     });
 
     options.inputGainType = parseInt(options.inputGainType as any) as 0 | 1;
@@ -81,26 +90,33 @@ const processFile = async (fileBuffer: Buffer, status: SourceGroupStatus[], opti
     return projectFile;
 }
 
+function base64ToArrayBuffer(base64: string) {
+    var binaryString = atob(base64);
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
 Alpine.data<AppData, unknown[]>('app', () => ({
     projectFile: undefined,
+    customTemplateFileName: '',
     isDragOver: false,
     fileName: undefined,
     sourceGroupsStatus: undefined,
     projectOptions: { ...DEFAULT_OPTIONS },
     fileBuffer: undefined,
-    isVisible: true,
+    isVisible: false,
 
     init() {
-        fetch(TEMPLATES).then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
+        const arrayBuffer = base64ToArrayBuffer(templateBase64);
 
-            response.arrayBuffer().then(async (arrayBuffer) => {
-                const buffer = new Uint8Array(arrayBuffer)
+        const buffer = new Uint8Array(arrayBuffer)
 
-                templates = await AutoR1.AutoR1TemplateFile.build(Buffer.from(buffer))
-            });
+        AutoR1.AutoR1TemplateFile.build(Buffer.from(buffer)).then((res) => {
+            defaultTemplates = res;
+            templates = res;
         })
     },
 
@@ -110,15 +126,21 @@ Alpine.data<AppData, unknown[]>('app', () => ({
 
     resetStatus() {
         if (this.projectFile) {
-            this.sourceGroupsStatus = this.projectFile.sourceGroups.map(sg => ({ fallback: sg.fallback, mute: sg.mute, ds: sg.dsData }));
+            this.sourceGroupsStatus = this.projectFile.sourceGroups.map(sg => ({ fallback: sg.fallback, mute: sg.mute, ds: sg.dsData, eq: sg.eq }));
         }
         this.projectOptions = { ...DEFAULT_OPTIONS };
+        this.clearCustomTemplateFile();
     },
 
-    cleanFile() {
-        if (this.projectFile) {
-            const id = this.projectFile.getGroupIdFromName(GROUP_NAME);
-            this.projectFile.clean(id);
+    async cleanFile() {
+        if (this.fileBuffer) {
+            const projectFile = await AutoR1.AutoR1ProjectFile.build(this.fileBuffer!);
+            const id = projectFile.getGroupIdFromName(GROUP_NAME);
+            projectFile.clean(id);
+
+            downloadFile(projectFile, newCleanPath(this.fileName!));
+
+            alert('Cleaned project has been downloaded.');
         }
     },
 
@@ -131,18 +153,21 @@ Alpine.data<AppData, unknown[]>('app', () => ({
         // Clear fileInput
         const fileInput = document.getElementById('fileInput')! as HTMLInputElement;
         fileInput.value = '';
+
+        this.clearCustomTemplateFile();
     },
 
     statusHasChanged() {
-        return this.sourceGroupsStatus?.some((row) => !row.fallback || !row.mute || !row.ds) ||
+        return this.sourceGroupsStatus?.some((row) => !row.fallback || !row.mute || !row.ds || !row.eq) ||
             !this.projectOptions.main ||
             !this.projectOptions.meter ||
             !this.projectOptions.eq ||
             !this.projectOptions.arraySightControls ||
+            this.customTemplateFileName ||
             parseInt(this.projectOptions.inputGainType as any) !== DEFAULT_INPUT_GAIN_TYPE
     },
 
-    toggleAllStatus(prop: 'fallback' | 'mute' | 'ds') {
+    toggleAllStatus(prop: 'fallback' | 'mute' | 'ds' | 'eq') {
         if (this.sourceGroupsStatus) {
             this.sourceGroupsStatus = this.sourceGroupsStatus.map(row => ({ ...row, [prop]: !row[prop] }));
         }
@@ -151,7 +176,7 @@ Alpine.data<AppData, unknown[]>('app', () => ({
     toggleRowStatus(index: number) {
         if (this.sourceGroupsStatus) {
             const row = this.sourceGroupsStatus[index];
-            this.sourceGroupsStatus[index] = { fallback: !row.fallback, mute: !row.mute, ds: !row.ds };
+            this.sourceGroupsStatus[index] = { fallback: !row.fallback, mute: !row.mute, ds: !row.ds, eq: !row.eq };
         }
     },
 
@@ -167,7 +192,6 @@ Alpine.data<AppData, unknown[]>('app', () => ({
 
     handleDrop(event: DragEvent) {
         event.preventDefault();
-        console.log('handledrop')
         const droppedFile = event.dataTransfer ? event.dataTransfer.files[0] : (event.target as any).files[0];
 
         (document.getElementById('fileInput')! as HTMLInputElement).files = event.dataTransfer ? event.dataTransfer.files : (event.target as any).files;
@@ -194,7 +218,7 @@ Alpine.data<AppData, unknown[]>('app', () => ({
                 this.fileBuffer = Buffer.from(buffer);
                 this.fileName = droppedFile.name;
                 this.projectFile = projectFile;
-                this.sourceGroupsStatus = projectFile.sourceGroups.map(sg => ({ fallback: sg.fallback, mute: sg.mute, ds: sg.dsData }));
+                this.sourceGroupsStatus = projectFile.sourceGroups.map(sg => ({ fallback: sg.fallback, mute: sg.mute, ds: sg.dsData, eq: sg.eq }));
             } catch (e: any) {
                 alert(`AutoR1 has encountered the following error while processing ${droppedFile.name}:\n${e}`)
 
@@ -205,4 +229,54 @@ Alpine.data<AppData, unknown[]>('app', () => ({
 
         reader.readAsArrayBuffer(droppedFile);
     },
+
+    handleTemplateFileDrop(event: DragEvent) {
+        event.preventDefault();
+        debugger;
+        const droppedFile = event.dataTransfer ? event.dataTransfer.files[0] : (event.target as any).files[0];
+
+        (document.getElementById('templateInput')! as HTMLInputElement).files = event.dataTransfer ? event.dataTransfer.files : (event.target as any).files;
+
+        const reader = new FileReader();
+        reader.onload = async (event) => {
+            const fileContent = event.target!.result! as ArrayBuffer;
+            const buffer = new Uint8Array(fileContent);
+
+            try {
+                templates = await AutoR1.AutoR1TemplateFile.build(Buffer.from(buffer))
+                this.customTemplateFileName = droppedFile.name;
+            } catch (e: any) {
+                alert(`AutoR1 has encountered the following error while processing ${droppedFile.name}:\n${e}`)
+
+                this.clearFile();
+                return;
+            }
+        };
+
+        reader.readAsArrayBuffer(droppedFile);
+    },
+
+    clearCustomTemplateFile() {
+        templates = defaultTemplates;
+        this.customTemplateFileName = '';
+        const fileInput = document.getElementById('templateInput')! as HTMLInputElement;
+        fileInput.value = '';
+    },
+
+    downloadTemplateFile() {
+        const arraybuff = templates.db.export();
+
+        var blob = new Blob([arraybuff]);
+        const link = window.URL.createObjectURL(blob);
+
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        a.href = link;
+        a.download = 'templates.r2t';
+        a.click();
+        a.remove();
+
+        return link
+    }
+}));
 }));
